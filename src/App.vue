d424f8782140290e176f1ba562e24c8064621c4a
LXJ1546 <2991239755@qq.com>
2024-03-10 14:12:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="box">
+    <h1>我是app根组件{{ str }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '你是我的眼'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 100px;
+  height: 200px;
+}
+</style>
