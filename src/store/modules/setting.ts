e8326e291898146d83209b8b93d93a2d51f24d56
LXJ1546 <2991239755@qq.com>
2024-03-20 10:33:19 +0800

    内容实现

--- src/store/modules/setting.ts
+++ src/store/modules/setting.ts
@@ -2,8 +2,8 @@ import { defineStore } from 'pinia'
 const useSettingStore = defineStore('setting', {
   state: () => {
     return {
-      fold: false,// 菜单是否折叠
-      refresh:false,// 是否点击刷新按钮
+      fold: false, // 菜单是否折叠
+      refresh: false, // 是否点击刷新按钮
     }
   },
 })
